7c9f61967a8edc2ccb9e51a277581e2a146fa633
wwlinuxoff@gmail.com <wwlinuxoff@gmail.com>
2025-04-10 18:30:42 +0500

    13

--- LESSON_GitHub_HOMEWORK-2/Program.cs
+++ LESSON_GitHub_HOMEWORK-2/Program.cs
@@ -1,9 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("10, World!");
-Console.WriteLine("11");
-Console.WriteLine("12");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("9");
+﻿Console.WriteLine();
