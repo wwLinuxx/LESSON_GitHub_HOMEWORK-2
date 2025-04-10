5d103ff0dd3097743f23bd4f9dab33a49f9f1b1a
wwlinuxoff@gmail.com <wwlinuxoff@gmail.com>
2025-04-10 18:30:29 +0500

    12

--- LESSON_GitHub_HOMEWORK-2/Program.cs
+++ LESSON_GitHub_HOMEWORK-2/Program.cs
@@ -2,7 +2,7 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("10, World!");
 Console.WriteLine("11");
-Console.WriteLine("Hello, World!");
+Console.WriteLine("12");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
